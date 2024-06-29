d916216da12fe5f858c9de7f023fe2f082a95b69
Elene Zurabiani <elene.zurabiani906@ens.tsu.ge>
2024-06-30 03:30:11 +0400

    heart-button

--- base/models.py
+++ base/models.py
@@ -2,6 +2,7 @@ from django.db import models
 class Items(models.Model):
     name = models.CharField(max_length=30)
     image = models.CharField(max_length=200)
-    price = models.IntegerField()
+    price = models.CharField(max_length=10)
     size = models.CharField(max_length=10)
+    colour = models.CharField(max_length=30)
     description = models.TextField(max_length=300)
